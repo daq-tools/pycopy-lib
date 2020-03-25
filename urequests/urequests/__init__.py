2fe5acb0cbd4b6fdfcb8889db5e9fc3f3d938aca
Andreas Motl <andreas@terkin.org>
2020-03-25 19:16:39 +0100

    urequests: Improve compatibility with CPython
    1. "method", "path" and "host" are all variables of regular string type,
       which doesn't interpolate well into bytestrings.
    
    2. The MicroPython "usocket" documentation says:
    
    > For efficiency and consistency, socket objects in MicroPython
    > implement a stream (file-like) interface directly. In CPython,
    > you need to convert a socket to a file-like object using
    > makefile() method. This method is still supported by MicroPython
    > (but is a no-op), so where compatibility with CPython matters,
    > be sure to use it.

--- urequests/urequests/__init__.py
+++ urequests/urequests/__init__.py
@@ -69,24 +69,26 @@ def request(method, url, data=None, json=None, headers={}, stream=None, parse_he
             s.connect(ai[-1])
             if proto == "https:":
                 s = ussl.wrap_socket(s, server_hostname=host)
-            s.write(b"%s /%s HTTP/1.0\r\n" % (method, path))
+            ss = s.makefile(mode='rwb')
+            ss.write(b"%s /%s HTTP/1.0\r\n" % (method.encode(), path.encode()))
             if not "Host" in headers:
-                s.write(b"Host: %s\r\n" % host)
+                ss.write(b"Host: %s\r\n" % host.encode())
             # Iterate over keys to avoid tuple alloc
             for k in headers:
-                s.write(k)
-                s.write(b": ")
-                s.write(headers[k])
-                s.write(b"\r\n")
+                ss.write(k.encode())
+                ss.write(b": ")
+                ss.write(headers[k].encode())
+                ss.write(b"\r\n")
             if json is not None:
-                s.write(b"Content-Type: application/json\r\n")
+                ss.write(b"Content-Type: application/json\r\n")
             if data:
-                s.write(b"Content-Length: %d\r\n" % len(data))
-            s.write(b"Connection: close\r\n\r\n")
+                ss.write(b"Content-Length: %d\r\n" % len(data))
+            ss.write(b"Connection: close\r\n\r\n")
             if data:
-                s.write(data)
+                ss.write(data)
+            ss.flush()
 
-            l = s.readline()
+            l = ss.readline()
             #print(l)
             l = l.split(None, 2)
             status = int(l[1])
@@ -94,7 +96,7 @@ def request(method, url, data=None, json=None, headers={}, stream=None, parse_he
             if len(l) > 2:
                 reason = l[2].rstrip()
             while True:
-                l = s.readline()
+                l = ss.readline()
                 if not l or l == b"\r\n":
                     break
                 #print(l)
